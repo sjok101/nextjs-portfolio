9823fef85b1ab27624bebda2d3f4667f64877c52
sjok <sjok101@gmail.com>
2025-04-07 00:47:15 -0400

    add styles to navbar

--- src/components/portfolio/NavBar.tsx
+++ src/components/portfolio/NavBar.tsx
@@ -1,17 +1,19 @@
 'use client'
 import Link from "next/link"
 
-export default function NavBar(){
-    return(
-        <div>
 
-            <Link href="#about" scroll={true}>
-                <span className="cursor-pointer">Contact</span>
+export default function NavBar(){
+    const navButtonStyle = "mx-3 flex justify-center items-center h-[4vh] w-[7vw] hover:bg-gradient-to-r from-sky-400 to-transparent text-white font-semibold text-opacity-90";
+    return(
+        <div className="mx-auto flex items-center h-[4vh] w-[70vw] bg-gradient-to-r from-sky-500 to-sky-700 border-blue-950 rounded">
+
+            <Link href="#about" scroll={true} className={navButtonStyle}>
+                <span className="cursor-pointer">About Me</span>
             </Link>
-            <Link href="#projects" scroll={true}>
-                <span className="cursor-pointer">Contact</span>
+            <Link href="#projects" scroll={true} className={navButtonStyle}>
+                <span className="cursor-pointer">Projects</span>
             </Link>
-            <Link href="#contact" scroll={true}>
+            <Link href="#contact" scroll={true} className={navButtonStyle}>
                 <span className="cursor-pointer">Contact</span>
             </Link>
 
