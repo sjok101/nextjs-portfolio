d015a4b00ad3aaa5c38080b05444d303687a31a9
sjok <sjok101@gmail.com>
2025-04-09 16:39:27 -0400

    applied preview effect

--- src/components/portfolio/ProjectDisplay.tsx
+++ src/components/portfolio/ProjectDisplay.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { IProject } from "@/models/projects";
 import Project from "./Project";
+import { useEffect, useState } from "react";
 
 export default function ProjectDisplay( {projects}:{ projects: IProject[] }){
 
-    projects.map((x) => console.log(x))
-
+    
+    const [preview, setPreview] = useState(false);
+    console.log(preview);
     return(
         <div>
             <p className="text-xl text-slate-200">Projects</p>
@@ -13,10 +15,12 @@ export default function ProjectDisplay( {projects}:{ projects: IProject[] }){
                             flex flex-row">
                 <ul>
                     {projects.map((project, i)=> (
-                        <li key={i}><Project project={project}/></li>
+                        <li key={i} onMouseEnter={() => setPreview(true)}
+                                    onMouseLeave={() => setPreview(false)}><Project project={project}/></li>
                     ))}
                 </ul>
-                <div id="preview" className=" ml-4 w-[244px] bg-slate-800/30">Preview</div>
+                <div id="preview" className={` ml-4 w-[244px] bg-slate-800/30
+                                               ${preview ? 'opacity-100' : 'opacity-0'} `}>Preview</div>
             </div>
         </div>
     )
