5a94e5d04551d0cc2fe014efb1edd688c6666c08
sjok <sjok101@gmail.com>
2025-04-09 16:16:34 -0400

    mapped projects

--- src/components/portfolio/ProjectDisplay.tsx
+++ src/components/portfolio/ProjectDisplay.tsx
@@ -4,14 +4,20 @@ import Project from "./Project";
 
 export default function ProjectDisplay( {projects}:{ projects: IProject[] }){
 
+    projects.map((x) => console.log(x))
+
     return(
-        <div className="border-black h-[600px] bg-slate-800 text-slate-300">
-            This is a Project Display section.
-            <ul>
-                <li><Project project={projects[0]}/></li>
-                <li><Project project={projects[1]}/></li>
-                <li><Project project={projects[2]}/></li>
-            </ul>
+        <div>
+            <p className="text-xl text-slate-200">Projects</p>
+            <div className="h-[800px] w-[800px] text-slate-300
+                            flex flex-row">
+                <ul>
+                    {projects.map((project, i)=> (
+                        <li key={i}><Project project={project}/></li>
+                    ))}
+                </ul>
+                <div id="preview" className=" ml-4 w-[244px] bg-slate-800/30">Preview</div>
+            </div>
         </div>
     )
 }
