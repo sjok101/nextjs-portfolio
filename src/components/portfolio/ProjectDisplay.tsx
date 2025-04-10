201baec5d486dc8a4101b923b382a2f71e025331
sjok <sjok101@gmail.com>
2025-04-09 21:16:06 -0400

    implemented image preview

--- src/components/portfolio/ProjectDisplay.tsx
+++ src/components/portfolio/ProjectDisplay.tsx
@@ -2,12 +2,13 @@
 import { IProject } from "@/models/projects";
 import Project from "./Project";
 import { useEffect, useState } from "react";
-
+import Image from 'next/image';
+import logo from "@/../public/230x125.svg"
 export default function ProjectDisplay( {projects}:{ projects: IProject[] }){
-
-    
+    //useStates to set values for previews and their index
     const [preview, setPreview] = useState(false);
-    console.log(preview);
+    const [index, setIndex] = useState(0);
+    
     return(
         <div>
             <p className="text-xl text-slate-200">Projects</p>
@@ -16,11 +17,19 @@ export default function ProjectDisplay( {projects}:{ projects: IProject[] }){
                 <ul>
                     {projects.map((project, i)=> (
                         <li key={i} onMouseEnter={() => setPreview(true)}
-                                    onMouseLeave={() => setPreview(false)}><Project project={project}/></li>
+                                    onMouseLeave={() => setPreview(false)}
+                                    onMouseOver={() => setIndex(i)}><Project project={project}/></li>
                     ))}
                 </ul>
                 <div id="preview" className={` ml-4 w-[244px] bg-slate-800/30
-                                               ${preview ? 'opacity-100' : 'opacity-0'} `}>Preview</div>
+                                               ${preview ? 'opacity-100' : 'opacity-0'} `}>
+                    <div>{projects[index].name}</div>
+                    <ul>
+                        {projects[index].link.map((img, i) => (
+                            <li key={i}><Image src={img} alt ={"Preview"} width={230} height={125}></Image></li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     )
--- src/models/projects.ts
+++ src/models/projects.ts
@@ -4,14 +4,14 @@ import mongoose, { Schema, Document, models, model } from 'mongoose';
 export interface IProject extends Document {
 
     name: string;
-    link: string;
+    link: string[];
     description: string;
 }
 
 const ProjectSchema: Schema = new Schema<IProject>({
 
     name: {type: String, required: true},
-    link: {type: String, required: true},
+    link: [{type: String, required: true}],
     description: {type: String, required: true}
 })
 
