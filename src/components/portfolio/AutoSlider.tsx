c90862a28df0ed8e5ef0a242de774444893cb857
sjok <sjok101@gmail.com>
2025-04-10 14:51:24 -0400

    applied link from prop

--- src/components/portfolio/AutoSlider.tsx
+++ src/components/portfolio/AutoSlider.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { IProject } from "@/models/projects";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 import Image from "next/image"
 export default function AutoSlider( {projects} : {projects: IProject[]} ){
     
@@ -15,29 +16,27 @@ export default function AutoSlider( {projects} : {projects: IProject[]} ){
                 setCount(prev => (prev+1) % projects.length);
                 setFade(true);
             }, 500);
-        }, 10000);
+        }, 20000);
 
         return () => clearInterval(interval);
     }, [projects.length]); //handles all projects
 
-    useEffect(() => {
-        if (count<0){
-            setCount(2);
-        }
-    }, [count]);
+
 
     return(
         <div className="z-10 flex flex-row items-center">
             {/* set >0 due to state updates */}
-            <button onClick={() => setCount(prev => count>0 ? (prev-1) % projects.length : 2)}
+            <button onClick={() => setCount(prev => count>0 ? (prev-1) % projects.length : projects.length-1)}
                     className={buttonStyle}>Left</button>
         <div className="h-[400px] w-[900px] border relative transition duration-500 ease-in-out mx-auto">
             <div 
             key={projects[count]._id as string}
-            className={`h-[400px] w-[900px] flex justify-center transition-opacity duration-500 bg-slate-900/50 ${fade ? 'opacity-100' : 'opacity-0'}`}
+            className={`h-[400px] w-[900px] flex justify-center  transition-opacity duration-500 bg-slate-900/50 ${fade ? 'opacity-100' : 'opacity-0'}`}
             >
-                <Image src={projects[count].frontPreview} alt="AutoSlidePreview" height={500} width={700}></Image>
-                <div className="flex flex-col self-start w-[200px]">
+                <Link href={projects[count].frontPreviewUrl} target="_blank" className="block">
+                    <img src={projects[count].frontPreview} alt="AutoSlidePreview" height={500} width={700}></img>
+                </Link>
+                <div className="ml-1 flex flex-col self-start w-[200px]">
                     <h3 className = "text-xl font-semibold text-slate-100">{projects[count].name}</h3>
                     <p className="text-slate-100/90 ">{projects[count].description}</p>
                 </div>
