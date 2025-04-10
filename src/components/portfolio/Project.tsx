8fc0db2ce5d77673409f7ff9affa291a1210b24b
sjok <sjok101@gmail.com>
2025-04-10 14:11:33 -0400

    edit dimension, make bigger

--- src/components/portfolio/Project.tsx
+++ src/components/portfolio/Project.tsx
@@ -2,7 +2,7 @@ import { IProject } from "@/models/projects";
 
 export default function Project( {project}:{ project: IProject }){
     return(
-        <div className="mb-2 h-[100px] w-[650px] bg-slate-800/50">
+        <div className="mb-2 h-[120px] w-[700px] bg-slate-800/50">
 
             This is a {project.name}.
         </div>
--- src/components/portfolio/ProjectDisplay.tsx
+++ src/components/portfolio/ProjectDisplay.tsx
@@ -3,7 +3,7 @@ import { IProject } from "@/models/projects";
 import Project from "./Project";
 import { useEffect, useState } from "react";
 import Image from 'next/image';
-import logo from "@/../public/230x125.svg"
+
 export default function ProjectDisplay( {projects}:{ projects: IProject[] }){
     //useStates to set values for previews and their index
     const [preview, setPreview] = useState(false);
@@ -12,7 +12,7 @@ export default function ProjectDisplay( {projects}:{ projects: IProject[] }){
     return(
         <div>
             <p className="text-xl text-slate-200">Projects</p>
-            <div className="w-[1000px] bg-slate-900/20 text-slate-300
+            <div className="w-[1200px] bg-slate-900/20 text-slate-300
                             flex flex-row p-3">
                 <ul>
                     {projects.map((project, i)=> (
@@ -22,12 +22,12 @@ export default function ProjectDisplay( {projects}:{ projects: IProject[] }){
                     ))}
                 </ul>
                 <div id="preview" className={` flex flex-col self-start
-                                                ml-4 w-[344px] h-auto bg-slate-800/30
+                                                ml-4 w-[494px] h-auto bg-slate-800/30
                                                ${preview ? 'opacity-100' : 'opacity-0'} `}>
                     <div>{projects[index].name}</div>
                     <ul>
                         {projects[index].link.map((img, i) => (
-                            <li key={i} className="py-5 px-3.5"><Image src={img} alt ={"Preview"} width={330} height={175}></Image></li>
+                            <li key={i} className="py-5 px-3.5"><Image src={img} alt ={"Preview"} width={480} height={225}></Image></li>
                         ))}
                     </ul>
                 </div>
