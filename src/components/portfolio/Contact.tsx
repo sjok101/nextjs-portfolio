19e73f3d8b717b317b80996255ae9d40e6028f2b
sjok <sjok101@gmail.com>
2025-04-10 00:28:00 -0400

    initial styling

--- src/components/portfolio/Contact.tsx
+++ src/components/portfolio/Contact.tsx
@@ -3,19 +3,20 @@
 import { handleSubmit } from "@/app/actions";
 
 export default function Contact(){
-    
+    const textFieldStyle = 'mb-5 bg-slate-100'
     
     return(
         //implement a contact form
         //send the data via router
-        <div className="bg-slate-800 text-slate-300">
-            <form action={handleSubmit}>
+        <div className="bg-slate-900/50 text-slate-800/90 h-[600px] w-[800px]">
+            <form action={handleSubmit} className="m-4 flex flex-col bg-slate-600/10">
             
                 <input
                     type="text"
                     name="name"
                     placeholder="Your Name"
                     required
+                    className={`${textFieldStyle} w-[300px]`}
                     />
 
                 <input
@@ -23,6 +24,7 @@ export default function Contact(){
                     name="email"
                     placeholder="Your E-Mail"
                     required
+                    className={`${textFieldStyle} w-[300px]`}
                     />
                 
                 <input
@@ -30,8 +32,9 @@ export default function Contact(){
                     name="message"
                     placeholder="Your Message"
                     required
+                    className={`${textFieldStyle} h-[400px]`}
                     />
-                <button type="submit">Submit</button>
+                <button type="submit" className={` text-2xl bg-slate-300/70 text-slate-900/90 h-[50px] rounded`}>Send Message</button>
             </form>
         </div>
     )
