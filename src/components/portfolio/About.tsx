605bc0585d966b4f2d1e180af70b1234722ba043
sjok <sjok101@gmail.com>
2025-04-10 00:41:55 -0400

    added section title

--- src/components/portfolio/About.tsx
+++ src/components/portfolio/About.tsx
@@ -39,24 +39,27 @@ export default function About( {aboutMe} : {aboutMe: IAboutMe | null} ){
     
     //applied fade to both container and text, transition to container only
     return(
-        <div className="z-10 flex flex-row items-center">
-            <button 
-            onClick={() => setLeftClicked(true)}
-            id="about-left" className={buttonStyle}>Left</button>
-        <div className= {`border-black bg-slate-900/50 bg-opacity-80 text-slate-300
-                         h-[600px] w-[1000px] flex flex-row items-start
-                         transition-opacity ease-in-out duration-500 ${fade ? 'opacity-0' : 'opacity-100'}`}>
-            
-            <img src="https://www.placehold.co/800x600"></img>
-            <div className={`w-[250px] text-center
-                             ${fade ? 'opacity-0' : 'opacity-100'}`}>
-                <p className="text-left p-5 pt-7">{aboutMe!.heading[count]}</p>
-                <p className="text-left px-5">{aboutMe!.description[count]}</p>
+        <div>
+            <h1 className="ml-[76px] text-xl text-slate-100/90">About me</h1>
+            <div className="z-10 flex flex-row items-center">
+                <button 
+                onClick={() => setLeftClicked(true)}
+                id="about-left" className={buttonStyle}>Left</button>
+            <div className= {`border-black bg-slate-900/50 bg-opacity-80 text-slate-300
+                            h-[600px] w-[1000px] flex flex-row items-start
+                            transition-opacity ease-in-out duration-500 ${fade ? 'opacity-0' : 'opacity-100'}`}>
+                            
+                <img src="https://www.placehold.co/800x600"></img>
+                <div className={`w-[250px] text-center
+                                ${fade ? 'opacity-0' : 'opacity-100'}`}>
+                    <p className="text-left p-5 pt-7">{aboutMe!.heading[count]}</p>
+                    <p className="text-left px-5">{aboutMe!.description[count]}</p>
+                </div>
             </div>
-        </div>
-            <button 
-            onClick={() => setRightClicked(true)}
-            id="about-right" className={buttonStyle}>Right</button>
+                <button 
+                onClick={() => setRightClicked(true)}
+                id="about-right" className={buttonStyle}>Right</button>
+            </div>
         </div>
     )
 }
--- src/components/portfolio/Contact.tsx
+++ src/components/portfolio/Contact.tsx
@@ -8,34 +8,37 @@ export default function Contact(){
     return(
         //implement a contact form
         //send the data via router
-        <div className="bg-slate-900/50 text-slate-800/90 h-[600px] w-[800px]">
-            <form action={handleSubmit} className="m-4 flex flex-col bg-slate-600/10">
-            
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Your Name"
-                    required
-                    className={`${textFieldStyle} w-[300px]`}
-                    />
+        <div>
+            <h1 className="text-xl text-slate-100/90">Contact Form</h1>
+            <div className="p-1 bg-slate-900/50 text-slate-800/90 h-[600px] w-[800px]">
+                <form action={handleSubmit} className="m-4 flex flex-col bg-slate-600/10">
 
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Your E-Mail"
-                    required
-                    className={`${textFieldStyle} w-[300px]`}
-                    />
-                
-                <input
-                    type="text"
-                    name="message"
-                    placeholder="Your Message"
-                    required
-                    className={`${textFieldStyle} h-[400px]`}
-                    />
-                <button type="submit" className={` text-2xl bg-slate-300/70 text-slate-900/90 h-[50px] rounded`}>Send Message</button>
-            </form>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Your Name"
+                        required
+                        className={`${textFieldStyle} w-[300px]`}
+                        />
+
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your E-Mail"
+                        required
+                        className={`${textFieldStyle} w-[300px]`}
+                        />
+
+                    <input
+                        type="text"
+                        name="message"
+                        placeholder="Your Message"
+                        required
+                        className={`${textFieldStyle} h-[400px]`}
+                        />
+                    <button type="submit" className={` text-2xl bg-slate-300/70 text-slate-900/90 h-[50px] rounded`}>Send Message</button>
+                </form>
+            </div>
         </div>
     )
 }
