bb1f7cc65243b0fffe3311e18fe45ec059750daf
sjok <sjok101@gmail.com>
2025-04-14 07:34:28 -0400

    resize width 900 -> 1000, increase image +20

--- src/components/portfolio/SocialMedia.tsx
+++ src/components/portfolio/SocialMedia.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function SocialMedia(){
-    const socialMediaStyle = "h-60 w-60 bg-gradient-to-b from-sky-400 to-sky-900/90 block my-10 border p-5 text-slate-100/90 border-black"
+    const socialMediaStyle = "h-80 w-80 bg-gradient-to-b from-sky-400 to-sky-900/90 block my-10 border p-10 text-slate-100/90 border-black"
     return(
-        <div className="flex flex-row justify-between w-[900px]">
+        <div className="flex flex-row justify-between w-[1000px]">
             <Link href="https://www.github.com" target="_blank" className={socialMediaStyle}>
                 <Image src="images/github.svg" alt="Github Logo" width={240} height={240}></Image>
                 <span className="cursor-pointer">GitHub</span>
