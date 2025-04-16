f866839e5c9b0488e73adada59b5da330a2796d6
sjok <sjok101@gmail.com>
2025-04-16 00:33:04 -0400

    implemented banner into project

--- src/components/portfolio/Project.tsx
+++ src/components/portfolio/Project.tsx
@@ -1,10 +1,13 @@
 import { IProject } from "@/models/projects";
-
+import Image from 'next/image';
 export default function Project( {project}:{ project: IProject }){
     return(
-        <div className="mb-2 h-[120px] w-[700px] bg-slate-800/50">
-
-            This is a {project.name}.
+        <div className="mb-2 h-[120px] w-[700px] bg-slate-800/50
+                        flex flex-row">
+            <Image src={project.banner} alt="Project Banner" height={120} width={285}></Image>
+            <div id="description" className="ml-2">
+                {project.name}
+            </div>
         </div>
     )
 }
