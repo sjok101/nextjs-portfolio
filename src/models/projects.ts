41ea2a2db4b10e12ceac882eac29a9c40987714d
sjok <sjok101@gmail.com>
2025-04-16 00:32:39 -0400

    add banner field

--- src/models/projects.ts
+++ src/models/projects.ts
@@ -5,6 +5,7 @@ export interface IProject extends Document {
 
     name: string;
     link: string[];
+    banner: string;
     frontPreview: string;
     frontPreviewUrl: string;
     description: string;
@@ -14,6 +15,7 @@ const ProjectSchema: Schema = new Schema<IProject>({
 
     name: {type: String, required: true},
     link: [{type: String, required: true}],
+    banner: {type: String, required: true},
     frontPreview: {type: String, required: true},
     frontPreviewUrl: {type: String, required: true},
     description: {type: String, required: true}
