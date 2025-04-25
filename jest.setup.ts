33eb659cad9a58d53f30afa2499ef1ee3c4d81bc
sjok <sjok101@gmail.com>
2025-04-25 03:24:45 -0400

    added whatwg-fetch for jsdom-jest fix https://github.com/vercel/next.js/discussions/59041

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,4 @@
 import { TextEncoder, TextDecoder } from 'util';
-
+import 'whatwg-fetch';
 global.TextEncoder = TextEncoder as typeof global.TextEncoder;
 global.TextDecoder = TextDecoder as typeof global.TextDecoder;
