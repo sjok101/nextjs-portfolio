9724bcc8d400e6f1dc2f293508f47c68aff161d5
sjok <sjok101@gmail.com>
2025-04-25 05:09:14 -0400

    fixed intermittent test failure

--- __tests__/models/aboutMe.test.ts
+++ __tests__/models/aboutMe.test.ts
@@ -4,19 +4,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 beforeAll(async () => {
+  
     const uri = process.env.MONGODB_URI;
     if (!uri) throw new Error('Missing MONGODB_URI env variable');
     
     await mongoose.connect(uri, {
       dbName: 'test',
     });
-  });
-  
-  afterEach(async () => {
     await AboutMe.deleteMany({});
   });
   
   afterAll(async () => {
+    await AboutMe.deleteMany({});
     await mongoose.disconnect();
   });
   
